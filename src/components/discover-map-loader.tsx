b68f54031d593f323f4e9b71df6eec42381b13b2
Robinson Cajas <robincajas2020@gmail.com>
2025-12-29 12:54:19 +0000

    Try fixing this error: `Runtime Error: Map container is already initiali

--- src/components/discover-map-loader.tsx
+++ src/components/discover-map-loader.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import { useMemo } from "react";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
 import type { Event } from "@/lib/types";
 
+const EventMap = dynamic(() => import('@/components/event-map'), {
+    loading: () => <Skeleton className="w-full h-full" />,
+    ssr: false
+});
+
 export default function DiscoverMapLoader({ events }: { events: Event[] }) {
-    const EventMap = useMemo(() => dynamic(() => import('@/components/event-map'), {
-        loading: () => <Skeleton className="w-full h-full" />,
-        ssr: false
-    }), []);
-    
     return <EventMap events={events} />;
 }
